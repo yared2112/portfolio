fdf235e8cf3d9e479584acb1130a49d9a2624a8f
yared2112 <teklewold.yared21@gmail.com>
2022-06-15 04:51:57 +0300

    completed task

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
@@ -8,7 +11,14 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Yared Teklewold</h1>
         <h5 className='text-light'>Frontend Developer</h5>
-        
+        <CTA />
+        <HeaderSocials />
+
+        <div className='me'>
+          <img src={ME} alt='me' />
+        </div>
+
+        <a href='#contact' className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
