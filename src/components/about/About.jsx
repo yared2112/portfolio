764705319f086a57afc257f9f36fdb658796e257
YaredT21 <teklewold.yared21@gmail.com>
2022-06-19 11:31:00 +0300

    Add files via upload

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,55 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FiAward} from 'react-icons/fi'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      
+      <div className="about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About File" />
+          </div> 
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <FiAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+
+            <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Clients</h5>
+              <small>Some</small>
+            </article>
+
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>5+ Completed </small>
+            </article>
+          </div>
+
+          <p>
+          Experienced Software Specialist with a demonstrated history of working in Frontend web development. 
+          Skilled in Python (Programming Language), Flutter, JavaScript, React, Data Science, Software Design, 
+          Software Development, and Artificial Intelligence (AI). Strong information technology professional 
+          with a Bachelor's degree focused in Computer Science from Adama Science and Technology.
+          </p>
+
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
