e2c2aba1e915b84daf69ac822375daa41138f9ff
YaredT21 <teklewold.yared21@gmail.com>
2022-06-20 06:58:55 +0300

    completed task

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {BsFacebook} from 'react-icons/bs'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsTwitter} from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="" className='footer__logo'>YARED</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://facebook.com"><BsFacebook/></a>
+        <a href="https://linkedin.com"><BsLinkedin/></a>
+        <a href="https://twitter.com"><BsTwitter/></a>
+      </div>
+
+      <div className="footer_copyright">
+        <small>&copy; YAREDs Portfolio</small>
+      </div>
+    </footer>
   )
 }
 
